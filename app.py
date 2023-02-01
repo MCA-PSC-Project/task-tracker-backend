7c1aacba32b6877d9a2b95e9c36b319f8dc60d81
Prashant Kumar <prashantbittu123@gmail.com>
2023-02-01 23:56:52 +0530

    env

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask
+import os
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask, request
+
+load_dotenv()  # loads variables from .env file into environment
 
 app = Flask(__name__)
+
+# gets variables from environment
+db_url = os.getenv("DATABASE_URL", "LOCAL_DATABASE_URL")
+db_connection = psycopg2.connect(db_url)
 
 
 @app.get("/")  # http://127.0.0.1:5000/
 def get_index():
     return "Welcome to task tracker app!!!"
+
+
+if __name__ == "__main__":
+    app.run()
