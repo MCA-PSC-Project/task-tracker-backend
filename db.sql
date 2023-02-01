4530b8093ff3602369d092c9578cf01d1db2fdd2
Prashant Kumar <prashantbittu123@gmail.com>
2023-02-01 22:34:57 +0530

    updated db.sql

--- db.sql
+++ db.sql
@@ -1,178 +1,114 @@
 begin;
-
 --------------TYPES----------------------
-
-create type "media__type" as enum('image');
-
-create table "media"(
-"id" int primary key,
-"path" varchar(50),
-"name" varchar(20),
-"mediatype" media__type
-);
-
-create type "gender__type" as enum (
-'male',
-'female',
-'others'
-);
-
-create type "status__type" as enum (
-'in-progress',
-'completed'
-);
-
-create type "task__type" as enum (
-'my_day',
-'planned',
-'assigned',
-'custom'
-);
-
-create type product__type as enum(
-'grocery',
-'stationery',
-'electronics',
-'others'
-);
-
-create type theme__type as enum(
-'color',
-'image',
-'none'
-);
-
-create type mode__type as enum(
-'white',
-'dark'
-);
-
-create type top_task__type as enum(
-'in-progress',
-'completed',
-'priority'
-);
-
-
-
+create type "media__type" as enum ('image');
+create type "gender__type" as enum ('male', 'female', 'other');
+create type "status__type" as enum ('in-progress', 'completed');
+create type "task__type" as enum ('my_day', 'planned', 'assigned', 'custom');
+create type "product__type" as enum ('grocery', 'stationery', 'electronics', 'other');
+create type "theme__type" as enum ('color', 'image', 'none');
+create type "mode__type" as enum ('white', 'dark');
+create type "top__task__type" as enum ('in-progress', 'completed', 'priority');
 -----------------------TABLES-------------------------------------------
-
-
-create table "user"(
-    "id" serial PRIMARY key,
-    "name" varchar(30) not null,
-    "email" VARCHAR(30) not null UNIQUE,
-    "phone" VARCHAR(13) UNIQUE DEFAULT null,
-    "password" varchar(30) not null,
+create table "media"(
+	"id" int primary KEY,
+	"name" varchar(20),
+	"path" varchar(50),
+	"mediatype" media__type
+);
+create table "users"(
+	"id" serial PRIMARY KEY,
+	"name" varchar not null,
+	"email" VARCHAR not null UNIQUE,
+	"phone" VARCHAR(13) UNIQUE DEFAULT NULL,
+	"password" varchar not null,
 	"dob" date not null,
-	"gender" gender__type not null,
+	"gender" gender__type,
 	"added_at" timestamp not null,
 	"updated_at" timestamp DEFAULT null,
 	"dp_id" int,
-	"trash" boolean default false,
-	foreign key("dp_id") references "media"("id")
+	"trash" boolean DEFAULT false,
+	FOREIGN KEY("dp_id") references "media"("id")
 );
-
-
-
 create table "lists"(
-    "id" serial PRIMARY key,
+	"id" serial PRIMARY KEY,
 	"user_id" integer,
-    "name" varchar(30) not null,
+	"name" varchar not null,
 	"added_at" timestamp not null,
-	"updated_at" timestamp default null,
-	foreign key("user_id") references "user"("id")
+	"updated_at" timestamp DEFAULT null,
+	FOREIGN KEY("user_id") references "users"("id")
 );
-
-
-
-
 create table "tasks"(
-"id" serial primary key,
-"user_id" integer,
-"description" varchar(400),
-"status" status__type,
-"added_at" timestamp not null,
-"updated_at" timestamp DEFAULT null,
-"plan_start_date" timestamp default null,
-"plan_end_date" timestamp DEFAULT null,
-"actual_end_date" timestamp default null,
-"duration" integer,
-"task_type" task__type,
-"notify" boolean default true,
-"repeat" boolean default false,
-"priority" boolean default false,
-"list_id" integer,
-foreign key("user_id") references "user"("id"),
-foreign key("list_id") references "lists"("id")
-);
-
-
-
-create table "subtasks"(
-"id" serial primary key,
-"user_id" integer,
-"task_id" integer,
-"title" varchar(200),
-"description" varchar(400),
-"status" status__type,
-"added_at" timestamp not null,
-"updated_at" timestamp DEFAULT null,
-"plan_start_date" timestamp default null,
-"plan_end_date" timestamp DEFAULT null,
-"actual_end_date" timestamp default null,
-"duration" integer,
-"task_type" task__type,
-"notify" boolean default true,
-"repeat" boolean default false,
-"priority" boolean default false,
-foreign key("user_id") references "user"("id"),
-foreign key("task_id") references "tasks"("id")
-);
-
-
-
-create table "assigner_task"(
-"assigner_user_id" integer,
-"assignee_user_id" integer,
-"task_id" integer,
-"assigned_at" timestamp not null,
-foreign key("assigner_user_id") references "user"("id"),
-foreign key("assignee_user_id") references "user"("id"),
-foreign key("task_id") references "tasks"("id"),
-unique("assignee_user_id","task_id")
-);
-
-
-
-
-
-create table "basket"(
-    "id" serial PRIMARY key,
+	"id" serial primary KEY,
 	"user_id" integer,
-    "product_name" varchar(30) not null,
-	"s_type" status__type,
+	"title" varchar,
+	"description" varchar,
+	"status" status__type,
 	"added_at" timestamp not null,
-	"updated_at" timestamp default null,
-	"p_type" product__type,
-	"repeat" boolean default true,
-	foreign key("user_id") references "user"("id")
+	"updated_at" timestamp DEFAULT null,
+	"plan_start_date" timestamp DEFAULT null,
+	"plan_end_date" timestamp DEFAULT null,
+	"actual_end_date" timestamp DEFAULT null,
+	"duration" integer,
+	"task_type" task__type,
+	"notify" boolean DEFAULT true,
+	"repeat" boolean DEFAULT false,
+	"priority" boolean DEFAULT false,
+	"list_id" integer,
+	FOREIGN KEY("user_id") references "users"("id"),
+	FOREIGN KEY("list_id") references "lists"("id")
 );
-
-
-
-
-create table "user_settings"(
+create table "subtasks"(
+	"id" serial primary KEY,
+	"user_id" integer,
+	"task_id" integer,
+	"title" varchar,
+	"description" varchar,
+	"status" status__type,
+	"added_at" timestamp not null,
+	"updated_at" timestamp DEFAULT null,
+	"plan_start_date" timestamp DEFAULT null,
+	"plan_end_date" timestamp DEFAULT null,
+	"actual_end_date" timestamp DEFAULT null,
+	"duration" integer,
+	"task_type" task__type,
+	"notify" boolean DEFAULT true,
+	"repeat" boolean DEFAULT false,
+	"priority" boolean DEFAULT false,
+	FOREIGN KEY("user_id") references "users"("id"),
+	FOREIGN KEY("task_id") references "tasks"("id")
+);
+create table "assigned_tasks"(
+	"assigner_user_id" integer,
+	"assignee_user_id" integer,
+	"task_id" integer,
+	"assigned_at" timestamp not null,
+	PRIMARY KEY("assignee_user_id", "task_id"),
+	FOREIGN KEY("assigner_user_id") references "users"("id"),
+	FOREIGN KEY("assignee_user_id") references "users"("id"),
+	FOREIGN KEY("task_id") references "tasks"("id")
+);
+create table "basket"(
+	"id" serial PRIMARY KEY,
+	"user_id" integer,
+	"product_name" varchar not null,
+	"status_type" status__type,
+	"added_at" timestamp not null,
+	"updated_at" timestamp DEFAULT null,
+	"completed_at" timestamp DEFAULT null,
+	"product_type" product__type,
+	"repeat" boolean DEFAULT false,
+	FOREIGN KEY("user_id") references "users"("id")
+);
+create table "users_settings"(
 	"user_id" integer,
 	"theme_type" theme__type,
-	"theme_color" varchar(10) default 'white',
-	"background_image_id" integer default null,
-    "confirm_deletion" boolean default false,
-	"top_task_type" top_task__type default 'in-progress',
-	"notify" boolean default true,
-	"mode" mode__type default 'white',
-	foreign key("user_id") references "user"("id"),
-	foreign key("background_image_id") references "media"("id")
+	"theme_color" varchar(10) DEFAULT 'white',
+	"background_image_id" integer DEFAULT null,
+	"confirm_deletion" boolean DEFAULT false,
+	"top_task_type" top__task__type DEFAULT 'in-progress',
+	"notify" boolean DEFAULT true,
+	"mode" mode__type DEFAULT 'white',
+	FOREIGN KEY("user_id") references "users"("id"),
+	FOREIGN KEY("background_image_id") references "media"("id")
 );
-
 end;
